be02c98fa07962b4e0c8994a5cbb12e05312f210
SketchBuch <sb.design.de@gmail.com>
2019-07-01 09:02:15 +0200

    feat: working updatestatusbar tests

---
+++ src/test/utils/update_statusbar.test.ts
@@ -0,0 +1,50 @@
+import * as vscode from 'vscode';
+import * as assert from 'assert';
+import * as sinon from 'sinon';
+import { beforeEach } from 'mocha';
+import updateStatusbar from '../../utils/update_statusbar';
+
+const getMockEditor = (languageId: string) => {
+  return { document: { languageId } } as vscode.TextEditor;
+};
+
+suite('updateStatusbar()', function() {
+  let mockButton: vscode.StatusBarItem;
+  let mockButtons: vscode.StatusBarItem[];
+  let spiedHide: sinon.SinonSpy<[], void>;
+  let spiedShow: sinon.SinonSpy<[], void>;
+
+  beforeEach(() => {
+    mockButton = { hide: () => {}, show: () => {} } as vscode.StatusBarItem;
+    mockButtons = [mockButton];
+    spiedHide = sinon.spy(mockButton, 'hide');
+    spiedShow = sinon.spy(mockButton, 'show');
+  });
+
+  test('btn.hide() called if editor is undefined', function() {
+    updateStatusbar(undefined, mockButtons);
+    assert(spiedHide.calledOnce);
+    assert(spiedShow.notCalled);
+  });
+
+  test('btn.hide() called if editor is not "javascript/typescript"', function() {
+    const editor = getMockEditor('css');
+    updateStatusbar(editor, mockButtons);
+    assert(spiedHide.calledOnce);
+    assert(spiedShow.notCalled);
+  });
+
+  test('btn.show() called if editor is "javascript"', function() {
+    const editor = getMockEditor('javascript');
+    updateStatusbar(editor, mockButtons);
+    assert(spiedHide.notCalled);
+    assert(spiedShow.calledOnce);
+  });
+
+  test('btn.show() called if editor is "typescript"', function() {
+    const editor = getMockEditor('typescript');
+    updateStatusbar(editor, mockButtons);
+    assert(spiedHide.notCalled);
+    assert(spiedShow.calledOnce);
+  });
+});
